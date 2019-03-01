62b16def395184b71b71a7798922109a5b7795a1
GameChamer <admin@gamecharmer.com>
2019-03-01 08:07:48 -0500

    convertAscii function

--- src/Str.php
+++ src/Str.php
@@ -80,4 +80,52 @@ trait Str
     {
         return '*'.implode('*', $arr).'*';
     }
+
+    /**
+     * @param string $string
+     *
+     * @return string
+     */
+    public static function convertAscii(string $string)
+    {
+        // Replace Single Curly Quotes
+        $search[]  = chr(226).chr(128).chr(152);
+        $replace[] = "'";
+        $search[]  = chr(226).chr(128).chr(153);
+        $replace[] = "'";
+
+        // Replace Smart Double Curly Quotes
+        $search[]  = chr(226).chr(128).chr(156);
+        $replace[] = '"';
+        $search[]  = chr(226).chr(128).chr(157);
+        $replace[] = '"';
+
+        // Replace En Dash
+        $search[]  = chr(226).chr(128).chr(147);
+        $replace[] = '--';
+
+        // Replace Em Dash
+        $search[]  = chr(226).chr(128).chr(148);
+        $replace[] = '---';
+
+        // Replace Bullet
+        $search[]  = chr(226).chr(128).chr(162);
+        $replace[] = '*';
+
+        // Replace Middle Dot
+        $search[]  = chr(194).chr(183);
+        $replace[] = '*';
+
+        // Replace Ellipsis with three consecutive dots
+        $search[]  = chr(226).chr(128).chr(166);
+        $replace[] = '...';
+
+        // Apply Replacements
+        $string = str_replace($search, $replace, $string);
+
+        // Remove any non-ASCII Characters
+        $string = preg_replace("/[^\x01-\x7F]/","", $string);
+
+        return $string;
+    }
 }
